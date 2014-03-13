72d48ea6d306c5c3692c14d78e131245cc27432c
App Academy Student <student@appacademy.io>
2014-03-13 10:17:54 -0400

    organizes classes and methods

--- checkers.rb
+++ checkers.rb
@@ -1,3 +1,55 @@
 class Board
+  def intialize
+    create_grid
+    fill_grid
+  end
+
+  def create_grid
+    @rows = Array.new(8) {Array.new}
+  end
+
+  def fill_grid
+    @rows.each |row| do
+      row.each do |square|
+        #fill every other square
+      end
+    end
+  end
+
+  def display
+    @rows.each |row| do
+      rows.each do |piece|
+        if piece.nil?
+          print "| |"
+        elsif piece.king
+          print "| |"
+        else
+          print "|C|"
+        end
+      end
+      puts
+    end
+    nil
+  end
+
+end
+
+class Piece
+  def initialize
+
+  end
+
+  def perform_slide
+
+  end
+
+  def perform_jump
+    #remove piece from board
+  end
+
+  def move_diffs  #
+
+  end
+
 
 end
